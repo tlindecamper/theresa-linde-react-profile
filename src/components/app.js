4600a36e951e7207a65237f6e6ba41ab4642f5ab
tlindecamper <therelan2001@yahoo.com>
2019-05-02 16:21:01 -0600

    added form on portfolio page

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,131 @@
 import React, { Component } from 'react';
-import moment from "moment";
+
+
+import {
+  BrowserRouter as Router,
+  Switch, 
+  Route
+
+} from 'react-router-dom';
+import axios from "axios";
+
+
+import NavigationContainer from './navigation/navigation-container';
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
+import PortfolioDetail from "./portfolio/portfolio-details";
+import PortfolioManager from "./pages/portfolio-manager";
+import Auth from "./pages/auth";
+
+import NoMatch from "./pages/no-match";
+
 
 export default class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN"
+    };
+
+    this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this)
+    this.handleUnsuccessfulLogin = this.handleUnsuccessfulLogin.bind(this)
+    this.handleSuccessfulLogOut = this.handleSuccessfulLogOut.bind(this)
+  }
+
+  handleSuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "LOGGED_IN"
+    })
+  }
+  
+  handleUnsuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
+    })
+  }
+
+  handleSuccessfulLogOut() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
+    })
+  }
+
+  checkLoginStatus() {
+    return axios
+      .get("https://api.devcamp.space/logged_in", {
+        withCredentials: true
+      })
+      .then(response => {
+        const loggedIn = response.data.logged_in;
+        const loggedInStatus = this.state.loggedInStatus;
+
+        if (loggedIn && loggedInStatus === "LOGGED_IN") {
+          return loggedIn;
+        } else if (loggedIn && loggedInStatus === "NOT_LOGGED_IN") {
+          this.setState({
+            loggedInStatus: "LOGGED_IN"
+          });
+        } else if (!loggedIn && loggedInStatus === "LOGGED_IN") {
+          this.setState({
+            loggedInStatus: "NOT_LOGGED_IN"
+          });
+        }
+      })
+      .catch(error => {
+        console.log("Error", error);
+      });
+  }
+
+  componentDidMount() {
+    this.checkLoginStatus();
+  }
+
+  authorizedPages() {
+  return [<Route key="portfolio-manager" path="/portfolio-manager" component={PortfolioManager}/>];     
+  } 
+  
+    
   render() {
+    
     return (
-      <div className='app'>        
-        <h2>Theresa Linde Portfolio</h2>
+      <div className='container'>   
+      <Router>
         <div>
-          {moment().format('MMMM Do YYYY, h:mm:ss a')}
-        </div>
-      </div>
-    );
+          
+          <NavigationContainer 
+          loggedInStatus={this.state.loggedInStatus}
+          handleSuccessfulLogOut={this.handleSuccessfulLogOut}
+          />  
+
+          
+          
+          <Switch>
+            <Route exact path="/" component={Home}/>
+
+            <Route 
+            path="/auth" 
+            render={props => (
+              <Auth
+                {...props}
+                handleSuccessfulLogin={this.handleSuccessfulLogin}
+                handleUnsuccessfulLogin={this.handleUnsuccessfulLogin}
+                /> 
+              )}
+            />            
+
+            <Route path="/about-me" component={About}/> 
+            <Route path="/contact" component={Contact}/>
+            {this.state.loggedInStatus === "LOGGED_IN" ? (this.authorizedPages()) : null}
+            <Route path="/blog" component={Blog} />
+            <Route  exact path="/portfolio/:slug" component={PortfolioDetail} />
+            <Route component={NoMatch}/>
+          </Switch>
+        </div> 
+      </Router>
+    </div>
+  );
   }
 }
