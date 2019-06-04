78579a231450288da50134ba542dd7f9308ff7d5
tlindecamper <therelan2001@yahoo.com>
2019-06-04 17:21:43 -0600

    added blog functionality

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-export default function() {
+class Blog extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      blogItems: []
+    }
+
+    this.getBlogItems = this.getBlogItems.bind(this);    
+  }
+
+  getBlogItems() {
+    axios.get("https://theresalinde.devcamp.space/portfolio/portfolio_blogs", 
+    { withCredentials: true 
+    }).then(response => {
+      this.setState({
+        blogItems: response.data.portfolio_blogs
+      })
+    })
+    .catch(error => {
+      console.log("getBlogItems error", error);
+    });
+  }
+
+ componentWillMount() {
+   this.getBlogItems();
+ }
+
+  render() {
+    const blogRecords = this.state.blogItems.map(blogItem => {
+      return <h1>{blogItem.title}</h1>;
+    })
     return (
-        <div>
-          <h2>Blog</h2>
-    
           <div>
-            <Link to="/about-me">Read more about myself</Link>
+            {blogRecords}
           </div>
-        </div>
-      );
-}
+        );
+      }
+  }  
+
+  export default Blog
