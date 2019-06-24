bef817c6f6168e5cb916d5c1da55496383f4b36a
tlindecamper <therelan2001@yahoo.com>
2019-06-24 15:45:18 -0600

    infinite scroll features

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -8,18 +8,37 @@ class Blog extends Component {
     super();
 
     this.state = {
-      blogItems: []
+      blogItems: [],
+      totalCount: 0,
+      currentPage: 0
     }
 
-    this.getBlogItems = this.getBlogItems.bind(this);    
+    this.getBlogItems = this.getBlogItems.bind(this); 
+    this.activateInfiniteScroll();   
   }
 
+ activateInfiniteScroll() {
+    window.onscroll = () => {
+    
+    if (window.innerHeight + document.documentElement.scrollTop === 
+      document.documentElement.offsetHeight ) {
+      console.log("get more posts");
+    } 
+    }
+ }
+
   getBlogItems() {
+    this.setState({
+      currentPage: this.state.currentPage + 1
+    });
+
     axios.get("https://theresalinde.devcamp.space/portfolio/portfolio_blogs", 
     { withCredentials: true 
     }).then(response => {
+      
       this.setState({
-        blogItems: response.data.portfolio_blogs
+        blogItems: response.data.portfolio_blogs,
+        totalCount: response.data.meta.total_records,
       })
     })
     .catch(error => {
