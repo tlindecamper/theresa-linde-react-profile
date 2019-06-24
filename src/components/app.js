fd75f2b9fb6ebef2a52b4c0e5e12a0e215cf84d6
tlindecamper <therelan2001@yahoo.com>
2019-06-24 16:05:33 -0600

    added loading spinner

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Switch,  Route } from 'react-router-dom';
 import axios from "axios";
 import { library} from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash, faSignOutAlt, faEdit } from "@fortawesome/free-solid-svg-icons"
+import { faTrash, faSignOutAlt, faEdit, faSpinner } from "@fortawesome/free-solid-svg-icons"
 
 
 import NavigationContainer from './navigation/navigation-container';
@@ -19,7 +19,7 @@ import PortfolioManager from "./pages/portfolio-manager";
 import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
 
-library.add(faTrash, faSignOutAlt, faEdit);
+library.add(faTrash, faSignOutAlt, faEdit, faSpinner);
 
 
 export default class App extends Component {
--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from 'axios';
 import BlogItem from "../blog/blog-item";
 
@@ -10,7 +11,8 @@ class Blog extends Component {
     this.state = {
       blogItems: [],
       totalCount: 0,
-      currentPage: 0
+      currentPage: 0,
+      isLoading: true
     }
 
     this.getBlogItems = this.getBlogItems.bind(this); 
@@ -39,6 +41,7 @@ class Blog extends Component {
       this.setState({
         blogItems: response.data.portfolio_blogs,
         totalCount: response.data.meta.total_records,
+        isLoading: false
       })
     })
     .catch(error => {
@@ -56,8 +59,13 @@ class Blog extends Component {
     })
     
     return (
-        <div className="blog-container">     
-          <div className="content-container">{blogRecords} </div>    
+        <div className="blog-container">          
+          <div className="content-container">{blogRecords} </div>  
+
+            {this.state.isLoading ?  (
+            <div className="content-loader"> 
+              <FontAwesomeIcon icon="spinner" spin/>
+            </div> ) : null}   
         </div>
       )      
     }
