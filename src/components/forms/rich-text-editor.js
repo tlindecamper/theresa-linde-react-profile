983fb1fd1d57167073c0d3a23a9471041c8e60af
tlindecamper <therelan2001@yahoo.com>
2019-07-10 16:35:40 -0600

    added file/image uploader

--- src/components/forms/rich-text-editor.js
+++ src/components/forms/rich-text-editor.js
@@ -4,6 +4,7 @@ import { Editor } from "react-draft-wysiwyg";
 import draftToHtml from "draftjs-to-html";
 import htmlToDraft from "html-to-draftjs";
 
+
 export default class RichTextEditor extends Component {
     constructor(props ) {
         super(props);
@@ -13,8 +14,21 @@ export default class RichTextEditor extends Component {
         }
 
         this.onEditorStateChange = this.onEditorStateChange.bind(this);
+        this.getBase64 = this.getBase64.bind(this);
+        this.uploadFile = this.uploadFile.bind(this);
+    }
+    getBase64(file, callback) {
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => callback(reader.result)
+        reader.onerror = error => {};
     }
 
+    uploadFile(file) {
+        return new Promise((resolve, reject) => {
+            this.getBase64(file, data => resolve({ data: { link: data }}))
+        })
+    }
     onEditorStateChange(editorState) {
         this.setState({ editorState },
              this.props.handleRichTextEditorChange(
@@ -32,6 +46,18 @@ export default class RichTextEditor extends Component {
             wrapperClassName="demo-wrapper"
             editorClassname="demo-editor"
             onEditorStateChange={this.onEditorStateChange}
+            toolbar={{
+                inline: {inDropdown: true },
+                list: { inDropdown: true },
+                textAlign: { inDropdown: true },
+                history: {inDropdown: true},
+                image: {
+                    uploadCallback: this.uploadFile,
+                    alt: {present: true, mandatory:false},
+                    previewImage: true,
+                    inputAccept: "image/gif,image/jpeg,image/jpg,image/png,image/svg"
+                }
+            }}
           />
         </div>
         )
