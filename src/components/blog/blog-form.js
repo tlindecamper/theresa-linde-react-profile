78a98da92c05aace0575ac1301a5f7644a2a99e6
tlindecamper <therelan2001@yahoo.com>
2019-07-31 17:56:35 -0600

    blog updates

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -13,8 +13,11 @@ export default class BlogForm extends Component {
             title: "",
             blog_status: "",
             content: "",
-            featured_image: ""
+            featured_image: "",
+            apiUrl: "https://theresalinde.devcamp.space/portfolio/portfolio_blogs",
+            apiAction: "post"
         }
+
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleRichTextEditorChange = this.handleRichTextEditorChange.bind(this);
@@ -25,6 +28,7 @@ export default class BlogForm extends Component {
         this.deleteImage = this.deleteImage.bind(this);
         this.featuredImageRef = React.createRef();
     } 
+
     deleteImage(imageType) {
         axios
         .delete(
@@ -44,7 +48,12 @@ export default class BlogForm extends Component {
             this.setState({
                 id: this.props.blog.id,
                 title: this.props.blog.title,
-                status: this.props.blog.status
+                blog_status: this.props.blog.blog_status,
+                content: this.props.blog.content,
+                apiUrl: `https://theresalinde.devcamp.space/portfolio/portfolio_blogs/${
+                    this.props.blog.id
+                }`,
+                apiAction: "patch"
             })
         }
     }
@@ -89,11 +98,13 @@ export default class BlogForm extends Component {
     }
 
     handleSubmit(event) {
-        axios
-        .post("https://theresalinde.devcamp.space/portfolio/portfolio_blogs",
-         this.buildForm(),
-         {withCredentials: true}
-         )
+        axios({
+            method: this.state.apiAction,
+            url:this.state.apiUrl,
+            data:this.buildForm(),
+            withCredentials: true
+        })       
+         
          .then(response => {
             if (this.state.featured_image) {
                 this.featuredImageRef.current.dropzone.removeAllFiles()
@@ -104,9 +115,12 @@ export default class BlogForm extends Component {
                 content: "",
                 featured_image: ""   
             })
-            
+            if (this.props.editMode){
+                this.props.handleUpdateFormFormSubmission(response.data.portfolio_blog);
+            }else {
             this.props.handleSuccessfullFormSubmission(
-                response.data.portfolio_blog);             
+                response.data.portfolio_blog); 
+            }            
             })
             .catch(error => {
                 console.log("handleSubmit for blog error", error);
--- src/components/pages/blog-details.js
+++ src/components/pages/blog-details.js
@@ -15,7 +15,16 @@ export default class BlogDetail extends Component {
         }
         this.handleEditClick = this.handleEditClick.bind(this);
         this.handleFeaturedImageDelete = this.handleFeaturedImageDelete.bind(this);
+        this.handleUpdateFormSubmission = this.handleUpdateFormSubmission.bind(this)
     }
+
+    handleUpdateFormSubmission(blog) {
+        this.setState({
+            blogItem: blog,
+            editMode: false
+        })
+    }
+
     handleFeaturedImageDelete() {
         this.setState({
             blogItem: {
@@ -56,7 +65,13 @@ export default class BlogDetail extends Component {
 
         const contentManager = () => {
             if (this.state.editMode) {
-                return <BlogForm   handleFeaturedImageDelete={this.handleFeaturedImageDelete} editMode={this.state.editMode} blog={this.state.blogItem}/>
+                return <BlogForm  
+                 handleFeaturedImageDelete={this.handleFeaturedImageDelete}
+                 handleUpdateFormSubmission={this.handleUpdateFormSubmission}
+                 editMode={this.state.editMode}
+                 blog={this.state.blogItem}
+                 
+                 />
             } else {
                 return (
                 <div className="content-container">        
