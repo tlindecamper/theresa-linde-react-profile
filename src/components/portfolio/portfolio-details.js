cefa7d7126cf0433c8e1da79fd51e5fe62aa3dfb
tlindecamper <therelan2001@yahoo.com>
2019-08-02 14:34:47 -0600

    portfolio styling

--- src/components/portfolio/portfolio-details.js
+++ src/components/portfolio/portfolio-details.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 import Axios from "axios";
+import { timingSafeEqual } from "crypto";
 
 export default class PortfolioItems extends Component {
     constructor(props){
-      super(props)
+      super(props);
+      this.state = {
+        portfolioItem: {}
+      }
     }  
 
   componentWillMount() {
@@ -16,7 +20,9 @@ export default class PortfolioItems extends Component {
       { withCredentials: true }
     )
     .then(response => {
-      console.log("res", response);
+      this.setState({
+        portfolioItem: response.data.portfolio_item
+      })
     })
     .catch(error => {
       console.log("getportfolioitem error", error)
@@ -24,9 +30,43 @@ export default class PortfolioItems extends Component {
   }  
 
   render () {
+    const {
+    banner_image_url,
+    category,
+    description,
+    logo_url,
+    name,
+    thumb_image_url,
+    url 
+    } = this.state.portfolioItem;
+
+    const bannerStyles = {
+      backgroundImage: "url(" + banner_image_url +")",
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: 'center center',
+    }
+
+    const logoStyles = {
+      width: "200px",
+    }
+
     return (
-      <div>
-        <h2>Portfolio Detail for {this.props.match.params.slug}</h2>
+      <div className='portfoilio-detail-wrapper'>
+        <div className='banner' style={bannerStyles}>
+          <img src={logo_url}  style={logoStyles}/>
+        </div> 
+
+        <div className='portfolio-detail-description-wrapper'>
+          <div className='description'>{description}
+          </div>
+        </div>
+        <div className='bottom-content-wrapper'>
+          <a href={url} className='site-link' target="_blank" >
+            Visit {name}
+          </a>
+        </div>
+        
       </div>
     );
 }
